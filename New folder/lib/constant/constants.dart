a022eb5ec486f1ce88574dc973ec9191231701be
itsmisher <83750131+syedshaharayar@users.noreply.github.com>
2023-11-26 01:23:19 +0530

    Add files via upload

---
+++ New folder/lib/constant/constants.dart
@@ -0,0 +1,12 @@
+String mapBoxApiKey =
+    'pk.eyJ1Ijoic2F1cmFiaG13IiwiYSI6ImNreTRiYzNidjBhMTkydnB2dmpoeGt4ZmgifQ.2QZ4CsNiygDTAhkqASpbPg';
+
+String nftStorageApiKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDJmNGUwQTQrNTI4MkMyMDNkZDBEZmY2NUNlMkUwRTYyQUNCODFDRWUiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTYzNzkwNzQxNjEwNSwibmFtZSI6ImxhbmRfZG9jdW1lbnQifQ.5ReEuIxsDhWxOLa2lVe9n-B2PUjdEkwJ5jLsBGdBDGA';
+
+const String rpcUrl = "http://127.0.0.1:7545";
+
+const String contractAddress =
+    "0xD6af79CcaaCc6e1d747909d7580630aFc69Ff0B8"; //"0xbc6d9e15255533C19f1589A7e9fbB60440a539e0";
+
+const int chainId = 1337; // local ganache-1337
